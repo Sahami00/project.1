323e57c9313f2e10172e7b9f686b5e81ce78c027
Sahami00 <149586960+Sahami00@users.noreply.github.com>
2023-12-15 19:16:26 +0330

    Add files via upload

---
+++ p4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+ using namespace std;
+ 
+
+int main( ) {
+	char letter;
+	cout<< "please enter an English character in lower case/n";
+	cout>> letter;
+	
+	switch (letter){
+		case 'a':
+		cout<<"VOWEL";
+		break;
+		
+		case 'e':
+			cout<<"VOWEL";
+			break;
+			
+			
+			case'i':
+				cout<<"VOWEL";
+				break;
+				
+				case'o':
+					cout<<"VOWEL";
+					break;
+					
+					case'p':
+						cout<<"VOWEL";
+	}
+	 
+}
